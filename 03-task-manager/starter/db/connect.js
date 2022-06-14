a67040e9898e752eea43e2fb1455763517c9c138
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-06-13 22:41:51 -0700

    Rm db

--- 03-task-manager/starter/db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const connectionString =
-  "mongodb+srv://nolive:9u20FXoRbTWKGrc6@nodeexpressprojects.7zkcy.mongodb.net/03-task-manager?retryWrites=true&w=majority"
-
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("CONNECTION IS SUCCESSFUL"))
-  .catch((err) => console.log(err))
