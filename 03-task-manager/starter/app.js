513ddfb5607e6584950f00cbd1db2534b675d68b
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-06-13 20:34:50 -0700

    Set up connection to db

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const app = express();
-const tasks = require("./routes/tasks");
+require("./db/connect")
+const express = require("express")
+const app = express()
+const tasks = require("./routes/tasks")
 
 // Middleware
 
-app.use(express.json());
+app.use(express.json())
 
 // Routes
 app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
+  res.send("Task Manager App")
+})
 
-app.use("/api/v1/tasks", tasks);
+app.use("/api/v1/tasks", tasks)
 
-const port = 3000;
+const port = 3000
 
-app.listen(port, console.log(`Listening on port ${port}`));
+app.listen(port, console.log(`Listening on port ${port}`))
--- 03-task-manager/starter/connect.js
+++
@@ -1,2 +0,0 @@
-const connectionString =
-  "mongodb+srv://nolive:<mongo123>@nodeexpressprojects.7zkcy.mongodb.net/?retryWrites=true&w=majority";
