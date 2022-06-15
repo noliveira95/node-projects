43d93938fcdf673422a0844469a401127a683c35
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-06-14 20:47:07 -0700

    Refactored connectDB

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,7 +1,8 @@
-require("./db/connect")
 const express = require("express")
 const app = express()
 const tasks = require("./routes/tasks")
+const connectDB = require("./db/connect")
+require("dotenv").config()
 
 // Middleware
 
@@ -16,6 +17,13 @@ app.use("/api/v1/tasks", tasks)
 
 const port = 3000
 
-app.listen(port, console.log(`Listening on port ${port}`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Listening on port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-// TESTING
+start()
