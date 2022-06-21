7ee4815226c15cd000671b622e1283c5c52d19de
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-06-20 20:35:20 -0700

    Get single task & delete

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -7,7 +7,7 @@ const getAllTasks = async (req, res) => {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
   } catch (error) {
-    res.status(500).json({ msg: error });
+    res.status(500).json({ error });
   }
 };
 // CREATE NEW TASK
@@ -16,20 +16,38 @@ const createTask = async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
   } catch (error) {
-    res.status(500).json({ msg: error });
+    res.status(500).json({ error });
   }
 };
 // GET A SINGLE TASK
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Task ID : ${taskID} is not found` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 // UPDATE TASK
 const updateTask = (req, res) => {
   res.send("Updated task");
 };
 // DELETE TASK
-const deleteTask = (req, res) => {
-  res.send("Deleted task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `Task ID : ${taskID} is not found` });
+    }
+    res.status(200).json({ msg: `Task ID : ${taskID} has been deleted` });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 module.exports = {
